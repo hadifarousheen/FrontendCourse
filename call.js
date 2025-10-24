cde780dfcdbaa33c40212762c4aa075f330cf2e7
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-10-24 15:14:53 +0530

    initial code of the frontend course

---
+++ call.js
@@ -0,0 +1,9 @@
+let name={
+    firstName:"Hadifa",
+    lastName:"Rousheen",
+    printFullName:function(){
+        console.log(this.firstName+" "+this.lastName)
+    }
+}
+
+name.printFullName();
