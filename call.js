4bc272aa88ca836ed9bf3f7b91d1bb6c5408e1a0
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-10-24 15:46:30 +0530

    call code

--- call.js
+++ call.js
@@ -1,9 +1,36 @@
 let name={
     firstName:"Hadifa",
     lastName:"Rousheen",
-    printFullName:function(){
-        console.log(this.firstName+" "+this.lastName)
-    }
+    // printFullName:function(){
+    //     console.log(this.firstName+" "+this.lastName)
+    // }
 }
 
-name.printFullName();
+// name.printFullName();
+let name2={
+    firstName:"Tom",
+    lastName:"Jerry"
+}
+//name2.printFullName();
+// name.printFullName.call(name2)
+//name2 => where this needs to be pointed to 
+// using call method we can do function borrowing
+
+
+// let printFullName=function(){
+//     console.log(this.firstName+" "+this.lastName)
+// }
+
+// printFullName.call(name);
+// printFullName.call(name2);
+
+// what if there are parameters for the function printFullName?
+
+
+let printFullName=function(homeTown){
+    console.log(this.firstName+" "+this.lastName+" from "+homeTown)
+}
+printFullName.call(name,"karimnagar")
+// the first parameter will alwasys be the reference to this variable
+
+printFullName.call(name2,"cartoon");
